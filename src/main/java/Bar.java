10bbee13e551518af3c9ad1fa9f5cf94d56abb7b
diogobabo <diogo.babo@gmail.com>
2021-12-08 19:05:46 +0000

    passo 3

--- src/main/java/Bar.java
+++ src/main/java/Bar.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 abstract public class Bar {
+    private List<BarObserver> observers;
+
     public abstract boolean isHappyHour();
     public void startHappyHour() {};
     public void endHappyHour() {};
+    public Bar(){
+        this.observers = new ArrayList<BarObserver>();
+    }
+    public void addObserver(BarObserver observer) {
+        observers.add(observer);
+    }
+    public void removeObserver(BarObserver observer) {
+        observers.remove(observer);
+    }
+    public void notifyObservers() {
+        for (BarObserver observer : observers)
+            if (isHappyHour()) observer.happyHourStarted(this);
+            else observer.happyHourEnded(this);
+    }
 }
--- src/main/java/StringBar.java
+++ src/main/java/StringBar.java
@@ -1,8 +1,22 @@
 public class StringBar extends Bar{
-
-
+    private boolean boll;
+    public  StringBar() {
+        this.boll = false;
+    }
     @Override
     public boolean isHappyHour() {
-        return false;
+        return boll;
+    }
+
+    @Override
+    public void startHappyHour() {
+        this.boll = true;
+        notifyObservers();
+    }
+
+    @Override
+    public void endHappyHour() {
+        this.boll = false;
+        notifyObservers();
     }
 }
